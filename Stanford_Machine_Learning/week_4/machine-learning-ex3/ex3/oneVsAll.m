fd660e34e1c0c522dd1d14ac9a0e9ce241e261e0
Diego Babativa <alexlord.17@gmail.com>
2019-02-26 20:25:38 -0500

    Sending OneVsAll-Classification

--- Stanford_Machine_Learning/week_4/machine-learning-ex3/ex3/oneVsAll.m
+++ Stanford_Machine_Learning/week_4/machine-learning-ex3/ex3/oneVsAll.m
@@ -54,7 +54,7 @@ X = [ones(m, 1) X];
 initial_theta = zeros(n + 1, 1);
 
  % Set options for fminunc
- options = optimset('GradObj', 'on', 'MaxIter', 50);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 % Run fmincg to obtain the optimal theta
 % This function will return theta and the cost 
@@ -63,13 +63,11 @@ for c=1:num_labels
 [theta] = ...
      fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
              initial_theta, options);
+
+all_theta(c,:)=theta;
+
 endfor
-
-
-
-
-
-
+%all_theta
 % =========================================================================
 
 
