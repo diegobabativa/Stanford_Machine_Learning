e9d44ef5c3c2a2b1aa3d20d309807381a17cd25a
Diego Babativa <alexlord.17@gmail.com>
2019-03-03 11:59:09 -0500

    Sengind All x4 Week_4 neural network

--- Stanford_Machine_Learning/week_4/machine-learning-ex3/ex3/predict.m
+++ Stanford_Machine_Learning/week_4/machine-learning-ex3/ex3/predict.m
@@ -25,17 +25,19 @@ X = [ones(m, 1) X];
 %
 p = zeros(num_labels,1)
 
-a1 = X;
+a1 = X; 
 z2 = a1 * Theta1';
-a2 = sigmoid(z2);
+z2_sigmoid = sigmoid(z2);
 
-a2 = [ones(size(a2, 1),1) a2];
+a2 = [ones(size(z2_sigmoid, 1),1) z2_sigmoid];
 z3 = a2 * Theta2';
 a3 = sigmoid(z3);
 
-[val, maxi] = max(a3);
+[val, maxi] = max(a3,[],2);
 
 p = maxi;
+
+size(p)
 
 % =========================================================================
 
